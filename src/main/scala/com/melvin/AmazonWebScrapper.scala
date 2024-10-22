38694c0bf08138d1768c575d21f1031a2b479d4d
Minesh Melvin <minesh.melvin@synthesis.co.za>
2024-10-22 12:23:00 +0530

    fixed the headers jumbling issue

--- src/main/scala/com/melvin/AmazonWebScrapper.scala
+++ src/main/scala/com/melvin/AmazonWebScrapper.scala
@@ -5,7 +5,7 @@ import com.melvin.ScrapperUtils.{extractProductData, writeToCSV, extractProductL
  * @author ${minesh.melvin}
  */
 object AmazonWebScrapper extends App {
-  private val searchUrl = "https://www.amazon.in/s?k=laptops"
+  private val searchUrl = "https://www.amazon.in/s?k=home+decor"
   println(s"Scraping Amazon search page: $searchUrl")
 
   //println(s"Headers are: " + getHeaders(searchUrl))
@@ -13,11 +13,12 @@ object AmazonWebScrapper extends App {
   // Step 1: Fetch the search page and extract product links
   fetchPage(searchUrl) match {
     case Some(doc) =>
-      val productLinks = extractProductLinks(doc) //.toList
+      val productLinks = extractProductLinks(doc).toList
 
       // Step 2: Scrape data from each product
-      val scrapedData = productLinks.flatMap(link => extractProductData(link)).toList
-      writeToCSV("scraped_data.csv", scrapedData, "@")
+      val scrapedData = productLinks.flatMap(link => extractProductData(link))
+      //val scrapedData = extractProductData(productLinks).toSeq
+      writeToCSV("scraped_data.csv", scrapedData, ",")
 
     case None =>
       println("Failed the scrape the search results page.")
--- src/main/scala/com/melvin/ScrapperUtils.scala
+++ src/main/scala/com/melvin/ScrapperUtils.scala
@@ -12,7 +12,7 @@ object ScrapperUtils {
   def fetchPage(url: String): Option[Document] = {
     //Fetch HTML content of the page
     Try(Jsoup.connect(url)
-      .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.38")
+      .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.37")
       .timeout(5000)
       .get()) match {
       case Success(doc) => Some(doc)
@@ -44,15 +44,16 @@ object ScrapperUtils {
         // Extract specific product information
         val fullInfo = Map(
           "title" -> doc.select("#productTitle").text(),
-          "price" -> doc.select("#corePriceDisplay_desktop_feature_div .a-price-whole").text(),
-          "reviews" -> doc.select("#acrCustomerReviewText.a-size-base").text(),
-          "soldBy" -> doc.select("#sellerProfileTriggerId").text(), // Adjust selector based on actual page structure
+          "price" -> doc.select("#corePriceDisplay_desktop_feature_div .a-price-whole").first().text(),
+          "reviews" -> doc.select("#acrCustomerReviewText.a-size-base").first().text(),
+          "soldBy" -> doc.select("#sellerProfileTriggerId").first().text(), // Adjust selector based on actual page structure
           "isAmazonChoice" -> {if(doc.select("#acBadge_feature_div .ac-badge-text-secondary ac-orange").text().toLowerCase().contains("choice")) "Yes" else "No"},
           "freeDelivery" -> {if(doc.select("#mir-layout-DELIVERY_BLOCK").text().toLowerCase().contains("free delivery")) "Yes" else "No"}, //".a-size-small.a-color-success" // Adjust based on actual page structure
+          "bestSeller" -> {if(doc.select("#zeitgeistBadge_feature_div").text().toLowerCase().contains("best seller")) "Yes" else "No"},
           "boxContents" -> doc.select("#witb-content-list span.a-list-item").text(),
           "productDescription" -> doc.select("#productDescription").text(),
           "features" -> doc.select("#productDetails_feature_div").text(),
-          "stars" -> doc.select("#acrPopover span.a-size-base.a-color-base").text(),
+          "stars" -> doc.select("#acrPopover span.a-size-base.a-color-base").first().text(),
           "socialProof" -> doc.select("#social-proofing-faceout-title-tk_bought").text(),
           "ratingsHistogram" -> doc.select("#cm_cr_dp_d_rating_histogram").text(),
           "availability" -> doc.select("#availability").text(),
@@ -74,20 +75,24 @@ object ScrapperUtils {
     }
   }
 
-  def writeToCSV(fileName: String, data: List[Map[String, String]], separator: String): Unit = {
+  def writeToCSV(fileName: String, data: Seq[Map[String, String]], separator: String): Unit = {
     val writer = new PrintWriter(new File(fileName))
 
     try{
       // Extract headers from the first Map
-      val headers = if(data.nonEmpty) data.head.keys else List.empty
+      //val headers = data.flatMap(_.keys).distinct
+      val headers = List("title","price","bestSeller","stars","reviews","socialProof","soldBy","isAmazonChoice","freeDelivery","availability","boxContents","productDescription",
+        "features","ratingsHistogram","url")
 
-      // Write headers=
+      // Write headers
       writer.println(s"sep=$separator\n" + headers.mkString(separator))
+      //writer.println(headers.mkString(separator))
 
       // Write data
       data.foreach{ row =>
-        val values = headers.map(header => row.getOrElse(header, ""))
+        val values = headers.map(header => row.getOrElse(header, "").replace(separator, " "))
         writer.println(values.mkString(separator))
+        //println(values.mkString(separator))
       }
     } finally {
       writer.close()
